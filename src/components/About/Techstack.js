289622a09ca5c73cb04f09e086aa6843f0ce1ef8
Kartik B Bhargav <kartikbhargav93@gmail.com>
2024-01-20 15:12:15 +0100

    Adding text to the icons and updating the logo

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -2,67 +2,82 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import { CgCPlusPlus } from "react-icons/cg";
 import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
   DiMongodb,
   DiPython,
   DiGit,
   DiJava,
+  DiAndroid,
+  DiLinux,
+  DiDocker,
+  DiBitbucket,
+  DiGithub,
 } from "react-icons/di";
 import {
   SiRedis,
-  SiFirebase,
-  SiNextdotjs,
-  SiSolidity,
   SiPostgresql,
+  SiPandas,
+  SiNumpy,
+  SiScipy
 } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus /> C++
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiJavascript1 />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <TbBrandGolang />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiNodejs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiReact />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiSolidity />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiGit />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiRedis />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostgresql />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
         <DiPython />
+        <div className="tech-description">Python</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <CgCPlusPlus />
+        <div className="tech-description">C++</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJava />
+        <div className="tech-description">Java</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+        <div className="tech-description">MongoDB</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+        <div className="tech-description">Git</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRedis />
+        <div className="tech-description">Redis</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostgresql />
+        <div className="tech-description">PostgreSQL</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiAndroid />
+        <div className="tech-description">Android</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiLinux />
+        <div className="tech-description">Linux</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiDocker />
+        <div className="tech-description">Docker</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGithub />
+        <div className="tech-description">Github</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPandas />
+        <div className="tech-description">Pandas</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNumpy />
+        <div className="tech-description">Numpy</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiScipy />
+        <div className="tech-description">Scipy</div>
       </Col>
     </Row>
   );
